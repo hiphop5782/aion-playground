272fdcb3a0a600765842f3f0d50ebdbf836b46c7
hiphop5782 <zdlsz@hanmail.net>
2022-06-28 15:23:08 +0900

    미구현 라벨 추가

--- src/components/ItemExtraction.vue
+++ src/components/ItemExtraction.vue
@@ -2,7 +2,7 @@
 <div class="container-fluid">
     <div class="row mt-4">
         <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-           <jumbotron-header title="강화석 추출 시뮬레이션" contents="확률은 인게임과 다르니 재미로만 즐겨주시기 바랍니다"></jumbotron-header>
+           <jumbotron-header title="강화석 추출 시뮬레이션(미구현)" contents="확률은 인게임과 다르니 재미로만 즐겨주시기 바랍니다"></jumbotron-header>
         </div>
     </div>
 
